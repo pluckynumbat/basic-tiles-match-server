a784f35efec298c28757e9f03dd25d902cf85076
Neehar Patil <pluckynumbat@gmail.com>
2025-06-12 17:04:04 +0530

    added a basic http handler for when a level is requested from the server

--- server/server.go
+++ server/server.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func getLevel(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "the level will be provided here \n")
+	fmt.Println("level was requested and provided")
+}
+
 func main() {
 	fmt.Println("Server Coming Soon...")
 }
