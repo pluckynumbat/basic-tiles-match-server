22a044cfe9f881d741de47cd49e30172b8012bd7
Neehar Patil <pluckynumbat@gmail.com>
2025-06-16 15:35:22 +0530

    send the level json string in the response to the get level request

--- server/server.go
+++ server/server.go
@@ -53,7 +53,7 @@ const levelString = `{
 }`
 
 func getLevel(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "the level will be provided here \n")
+	fmt.Fprintf(w, levelString)
 	fmt.Println("level was requested and provided")
 }
 
